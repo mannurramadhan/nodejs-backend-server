651870dd99654acd29ee441df949447919cb6757
mannurramadhan <mannurramadhan@gmail.com>
2022-01-25 06:04:54 +0700

    + add "Hello World" statement on french

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Bonjour le monde');
+});
+
 const PORT = 5000
 
 app.listen(PORT, console.log('Server running on port 5000'));
